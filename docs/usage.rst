d1bdc64158cb20eb160f6ebb9ea1b8e8c9dae2d6
Matthew Leingang <leingang@nyu.edu>
2017-06-21 07:16:43 -0400

    unfinished skeleton of module and usage

--- docs/usage.rst
+++ docs/usage.rst
@@ -5,3 +5,14 @@ Usage
 To use NYU Academic Calendar Processing in a project::
 
     import nyucal
+
+Use Cases
+=========
+
+* Get the academic calendar for a single semester as an ICS or CSV file
+
+    - from the command line
+    - from a GUI
+
+
+
--- nyucal/nyucal.py
+++ nyucal/nyucal.py
@@ -1,3 +1,38 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+class CalendarStore(object):
+    """Repository of academic calendars"""
+
+    def __init__(self, url=None):
+        """Initializer"""
+        pass
+
+    @property
+    def calendars(self):
+        """The list of available calendars"""
+        pass
+
+class Calendar(object):
+    """A single academic calendar"""
+    name = None
+
+    def __init__(self):
+        pass
+
+    @property
+    def events(self):
+        """The list of available events in a calendar."""
+        pass
+
+class Event(object):
+    """A single event on an academic calendar"""
+
+    date = None
+    name = None
+    description = None
+
+    def __init__(self):
+        pass
+
